009984ef09ba13d280eec50248fc18c76ca4eab0
Alan Hernández <alan.dalethb23@gmail.com>
2025-07-27 19:01:49 +0000

    la pantalla añadela entre vistaas tambien

--- src/components/kavexa/page-wrapper.tsx
+++ src/components/kavexa/page-wrapper.tsx
@@ -1,10 +1,19 @@
 'use client';
 
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const PageWrapper = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+  const pathname = usePathname();
+  const [key, setKey] = useState(pathname);
+
+  useEffect(() => {
+    setKey(pathname + Math.random());
+  }, [pathname]);
+
   return (
-    <div className={cn("animate-in fade-in-0 duration-500", className)}>
+    <div key={key} className={cn("animate-in fade-in-0 duration-500", className)}>
       {children}
     </div>
   );
